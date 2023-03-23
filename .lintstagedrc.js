bce5546d84ea6bb2e3f8b6365b9bf6d7d6359ffc
ujaineel <ujaineel@gmail.com>
2023-03-23 01:52:45 -0400

    Add husky git hooks

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+// .lintstagedrc.js
+module.exports = {
+  // If any ts/js(x) files changed.
+  "**/*.{ts,js}?x": [
+    // Execute tests related to the staged files.
+    "npm run test -- --passWithNoTests --bail --findRelatedTests",
+
+    // Run the typechecker.
+    // Anonymous function means: "Do not pass args to the command."
+    () => "tsc --noEmit",
+  ],
+};
