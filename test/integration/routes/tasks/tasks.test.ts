c7d66286355f5063acb8188480247d074ba1d75b
ujaineel <ujaineel@gmail.com>
2023-03-28 21:38:51 -0400

    Revert integration test

--- test/integration/routes/tasks/tasks.test.ts
+++ test/integration/routes/tasks/tasks.test.ts
@@ -2,21 +2,25 @@ import request from "supertest";
 import server, { app } from "../../../../src/index";
 import { RESPONSE_CODE } from "../../../../src/utils/types/response.types";
 import taskFixture from "../../../helper/fixtures/tasks/taskFixture.json";
-import createTaskDtoFixture from "../../../helper/fixtures/tasks/createTaskDtoFixture.json";
 import main from "../../../helper/setup/setup-db";
 import prismaClient from "../../../../src/configs/db.config";
-import { type Task } from "@prisma/client";
 
 describe("Tasks routes", () => {
+  beforeAll((done) => {
+    done();
+  });
+
   beforeEach(async () => {
     await main();
-    server.listen();
   }, 10000);
 
-  afterEach(async () => {
+  afterAll(async () => {
     await prismaClient.$disconnect();
+  }, 10000);
+
+  afterEach(() => {
     server.close();
-  });
+  }, 10000);
 
   describe("GET /:id", () => {
     const invalidId: string = "test-id";
@@ -46,40 +50,6 @@ describe("Tasks routes", () => {
 
       expect(statusCode).toEqual(RESPONSE_CODE.OK);
       expect(parsedText?.data).toEqual(taskFixture);
-    });
-  });
-
-  describe("POST /", () => {
-    it("should return 201 (Created) if proper req.body provided", async () => {
-      const { statusCode, text }: { statusCode: number; text: any } =
-        await request(app)
-          .post("/tasks/")
-          .send(createTaskDtoFixture)
-          .set("Content-Type", "application/json")
-          .set("Accept", "application/json");
-
-      const data: Task = JSON.parse(text)?.data;
-
-      expect(statusCode).toEqual(RESPONSE_CODE.CREATED);
-
-      const { text: fetchedTask }: { text: any } = await request(app).get(
-        `/tasks/${data.id}`
-      );
-      const taskFetched: Task = JSON.parse(fetchedTask)?.data;
-
-      expect(data).toEqual(taskFetched);
-    });
-
-    it("should return 400 (Bad Request) if req.body is not appropriate", async () => {
-      const { statusCode, text }: { statusCode: number; text: any } =
-        await request(app).post("/tasks/").send({});
-
-      const message = JSON.parse(text)?.message;
-
-      expect(statusCode).toEqual(RESPONSE_CODE.BAD_REQUEST);
-      expect(message).toEqual(
-        "Bad request. Please check if data provided is proper"
-      );
     });
   });
 });
