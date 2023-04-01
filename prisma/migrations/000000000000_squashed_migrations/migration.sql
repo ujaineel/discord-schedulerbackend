350f2178758564d9eb18a960b75f002655d759be
ujaineel <ujaineel@gmail.com>
2023-03-31 23:48:04 -0400

    Update migrate:diff script and add migration

--- prisma/migrations/000000000000_squashed_migrations/migration.sql
+++ prisma/migrations/000000000000_squashed_migrations/migration.sql
@@ -1,11 +1,10 @@
--- CreateEnum
-CREATE TYPE "UserType" AS ENUM ('LOCAL', 'GOOGLE', 'DISCORD', 'APPLE', 'MICROSOFT');
-
 -- CreateTable
 CREATE TABLE "Task" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "dueDate" TIMESTAMP(3),
     "title" VARCHAR(255) NOT NULL,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
@@ -13,33 +12,3 @@ CREATE TABLE "Task" (
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
-
--- CreateTable
-CREATE TABLE "Profile" (
-    "id" TEXT NOT NULL,
-    "bio" TEXT,
-    "type" "UserType" NOT NULL DEFAULT 'LOCAL',
-    "userId" TEXT NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
