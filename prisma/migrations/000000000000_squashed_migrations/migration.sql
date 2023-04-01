5e6c5d9ed29ef3439bd53feaa77f4c2c31189bca
Jaineel Upadhyay <32003330+ujaineel@users.noreply.github.com>
2023-03-31 23:16:16 -0400

    T 0004 delete task (#7)
    * T-0004 add functionality
    
    * Add unit and integration tests for deleteTask
    
    * Update schema to include Users
    
    * T-0004-Deal-With-CodeQL
    
    * Update env variable
    
    * T-0004 add update env script
    
    * T-0004 remove hardcoded credential
    
    * Build dotenv-vault

--- prisma/migrations/000000000000_squashed_migrations/migration.sql
+++ prisma/migrations/000000000000_squashed_migrations/migration.sql
@@ -6,6 +6,8 @@ CREATE TABLE "Task" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "dueDate" TIMESTAMP(3),
     "title" VARCHAR(255) NOT NULL,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
@@ -15,31 +17,17 @@ CREATE TABLE "Task" (
 );
 
 -- CreateTable
-CREATE TABLE "Profile" (
-    "id" TEXT NOT NULL,
-    "bio" TEXT,
-    "type" "UserType" NOT NULL DEFAULT 'LOCAL',
-    "userId" TEXT NOT NULL
-);
-
--- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
