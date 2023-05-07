2e7ce5098cced85728b1f5cb77bbbb536fb1b4db
ujaineel <ujaineel@gmail.com>
2023-05-07 17:12:58 -0400

    Ignore configFn

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { env } from "@configs/app.config";
 import { ENV } from "@utils/types/app.types";
 import { config } from "dotenv-vault-core";
 
+// istanbul ignore next
 export const configFn = (): void => {
   if (env !== ENV.LOCAL) {
     config();
